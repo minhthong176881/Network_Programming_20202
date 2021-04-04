4dd17b73faf03023cfe36d96d929ccf097506fa2
minhthong176881 <dominhthong99@gmail.com>
2021-04-04 21:02:11 +0700

    Update gitignore

---
+++ cmake-build-debug/CMakeFiles/cpptris.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/thomii/IT Stuffs/Study/20202/Network Programming/Capstone Project/cpptris/src/common.cpp" "/home/thomii/IT Stuffs/Study/20202/Network Programming/Capstone Project/cpptris/cmake-build-debug/CMakeFiles/cpptris.dir/src/common.cpp.o"
+  "/home/thomii/IT Stuffs/Study/20202/Network Programming/Capstone Project/cpptris/src/game.cpp" "/home/thomii/IT Stuffs/Study/20202/Network Programming/Capstone Project/cpptris/cmake-build-debug/CMakeFiles/cpptris.dir/src/game.cpp.o"
+  "/home/thomii/IT Stuffs/Study/20202/Network Programming/Capstone Project/cpptris/src/keyboard.cpp" "/home/thomii/IT Stuffs/Study/20202/Network Programming/Capstone Project/cpptris/cmake-build-debug/CMakeFiles/cpptris.dir/src/keyboard.cpp.o"
+  "/home/thomii/IT Stuffs/Study/20202/Network Programming/Capstone Project/cpptris/src/lobby.cpp" "/home/thomii/IT Stuffs/Study/20202/Network Programming/Capstone Project/cpptris/cmake-build-debug/CMakeFiles/cpptris.dir/src/lobby.cpp.o"
+  "/home/thomii/IT Stuffs/Study/20202/Network Programming/Capstone Project/cpptris/src/main.cpp" "/home/thomii/IT Stuffs/Study/20202/Network Programming/Capstone Project/cpptris/cmake-build-debug/CMakeFiles/cpptris.dir/src/main.cpp.o"
+  "/home/thomii/IT Stuffs/Study/20202/Network Programming/Capstone Project/cpptris/src/network/client.cpp" "/home/thomii/IT Stuffs/Study/20202/Network Programming/Capstone Project/cpptris/cmake-build-debug/CMakeFiles/cpptris.dir/src/network/client.cpp.o"
+  "/home/thomii/IT Stuffs/Study/20202/Network Programming/Capstone Project/cpptris/src/network/connection.cpp" "/home/thomii/IT Stuffs/Study/20202/Network Programming/Capstone Project/cpptris/cmake-build-debug/CMakeFiles/cpptris.dir/src/network/connection.cpp.o"
+  "/home/thomii/IT Stuffs/Study/20202/Network Programming/Capstone Project/cpptris/src/network/server.cpp" "/home/thomii/IT Stuffs/Study/20202/Network Programming/Capstone Project/cpptris/cmake-build-debug/CMakeFiles/cpptris.dir/src/network/server.cpp.o"
+  "/home/thomii/IT Stuffs/Study/20202/Network Programming/Capstone Project/cpptris/src/piece.cpp" "/home/thomii/IT Stuffs/Study/20202/Network Programming/Capstone Project/cpptris/cmake-build-debug/CMakeFiles/cpptris.dir/src/piece.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
